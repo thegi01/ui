ec8651d548674798122f33a7e74b09a2c24fa955
thegi <deokim77@gmail.com>
2016-08-09 15:59:02 +0900

    tabs-object, tabs-object-call, tabs-call 파일 정리

--- component/tabs/tabs-call.js
+++ component/tabs/tabs-call.js
@@ -36,123 +36,127 @@ getDataAttr = dataAttr[hasDataAttr].get;
 setDataAttr = dataAttr[hasDataAttr].set;
 
 /* Tabs Control */
-var tabsCtrl = {
-	init : function(items, direction, autoPlay){
-		this.items = items;
-		this.itemsLen = this.items.length;
-		if(direction) {
-			this.idx = getDataAttr.call(this, 'current');
-			this.direction = 'next';
-		};
-		if(autoPlay) {
-			this.auto = 'false';
-			this.timer = 3000;
-		};
-	},
-	tabs : function(evtType, target){
-		// var self = this;
-		isLower.addEvent(this, evtType, function(e){
-			var t = isLower.getTarget(e);
-			if(t.tagName == 'A') 						// e.preventDefault();
-				isLower.prevent(); 
-			if( target ) 								// get idx target 설정
-				t = t[target]; 
-			if(getDataAttr.call(t, 'role') != 'tabsItem')  	// 이벤트 타겟 검증
-				return;
-			if( this.auto ) {  							// auto play
-				clearInterval(this.interval);
-				tabsCtrl.play.call(this);
-			};
-			this.idx = Number( getDataAttr.call(t, 'idx') );
-			tabsCtrl.setCurrent.call(this, this.idx);
-		});
-	},
-	setCurrent : function(idx){
-		setDataAttr.call(this, 'current', idx);
-		if(isIE8)	// In IE8, css doesn't apply 
-			this.className = this.className;
-	},
-
-
-	prevNext : function(d, n){
-		this.direction = d;
-		if( this.auto ) {
+var setItems,
+	tabs,
+	setCurrent,
+	prevNext,
+	getIdxByDirection,
+	autoPlay,
+	play,
+	pause;
+setItems = function(items, direction, autoPlay){
+	this.items = items;
+	this.itemsLen = this.items.length;
+	if(direction) {
+		this.idx = getDataAttr.call(this, 'current');
+		this.direction = 'next';
+	};
+	if(autoPlay) {
+		this.auto = 'false';
+		this.timer = 3000;
+	};
+};
+tabs = function(evtType, target){
+	addEvent(this, evtType, function(e){
+		var t = getTarget(e);
+		if(t.tagName == 'A') 						// e.preventDefault();
+			prevent(); 
+		if( target ) 								// get idx target 설정
+			t = t[target]; 
+		if(getDataAttr.call(t, 'role') != 'tabsItem')  	// 이벤트 타겟 검증
+			return;
+		if( this.auto ) {  							// auto play
 			clearInterval(this.interval);
-			tabsCtrl.play.call(this);
+			play.call(this);
 		};
-		tabsCtrl.getIdxByDirection.call(this, n);
-		tabsCtrl.setCurrent.call(this, this.idx);
-	},
-	getIdxByDirection : function(n){
-		var d = this.direction,
-			idx = Number(this.idx),
-			val = this.itemsLen-1;
-		if( d == "prev" ){
-			if(n) 
-				idx = idx - n; 
-			else
-				idx--;
-			if( idx < 0 ) {
-				if(n) 
-					idx = Math.floor(val/n)*n;
-				else
-					idx = val;
-			};
-		} else {
-			if(n)
-				idx = idx + n;
-			else 
-				idx++;
-			if( idx > val ) 
-				idx = 0;
-		};
-		this.idx = idx;
-	},
-	autoPlay : function(timer){
-		if(timer) 
-			this.timer = timer;
-		this.auto = true;		
-		setDataAttr.call(this, 'auto', true);
-		tabsCtrl.play.call(this);
-	},
-	play : function(){
-		var el = this;
-		el.interval = setInterval(function(){
-			tabsCtrl.getIdxByDirection.call(el);
-			tabsCtrl.setCurrent.call(el, el.idx);
-		}, el.timer);
-	},
-	pause : function(){
+		this.idx = Number( getDataAttr.call(t, 'idx') );
+		setCurrent.call(this, this.idx);
+	});
+};
+setCurrent = function(idx){
+	setDataAttr.call(this, 'current', idx);
+	if(isIE8)	// In IE8, css doesn't apply 
+		this.className = this.className;
+};
+prevNext = function(d, n){
+	this.direction = d;
+	if( this.auto ) {
 		clearInterval(this.interval);
-		this.auto = false;
-		setDataAttr.call(this, 'auto', false);
-	}
+		play.call(this);
+	};
+	getIdxByDirection.call(this, n);
+	setCurrent.call(this, this.idx);
+};
+getIdxByDirection = function(n){
+	var d = this.direction,
+		idx = Number(this.idx),
+		val = this.itemsLen-1;
+	if( d == "prev" ){
+		if(n) 
+			idx = idx - n; 
+		else
+			idx--;
+		if( idx < 0 ) {
+			if(n) 
+				idx = Math.floor(val/n)*n;
+			else
+				idx = val;
+		};
+	} else {
+		if(n)
+			idx = idx + n;
+		else 
+			idx++;
+		if( idx > val ) 
+			idx = 0;
+	};
+	this.idx = idx;
+};
+autoPlay = function(timer){
+	if(timer) 
+		this.timer = timer;
+	this.auto = true;		
+	setDataAttr.call(this, 'auto', true);
+	play.call(this);
+};
+play = function(){
+	var el = this;
+	el.interval = setInterval(function(){
+		getIdxByDirection.call(el);
+		setCurrent.call(el, el.idx);
+	}, el.timer);
+};
+pause = function(){
+	clearInterval(this.interval);
+	this.auto = false;
+	setDataAttr.call(this, 'auto', false);
 };
 
+
 /* Lower Brower Supply */
-var isLower = {
-	getTarget : function(e){
-		return e.target || e.srcElement;
-	},
-	addEvent : function(e, type, handler){
-		if(isIE8 && type == 'change')
-			type = 'click';
-		if(e.attachEvent)
-			e.attachEvent("on" + type, handler);
-		else /*if(e.addEventListener)*/
-			e.addEventListener(type, handler);
-	},
-	prevent : function(){
-		if(event.preventDefault)
-			event.preventDefault();
-		else
-			event.returnValue = false;
-	}
+var getTarget,
+	addEvent,
+	prevent;
+getTarget = function(e){
+	return e.target || e.srcElement;
+};
+addEvent = function(e, type, handler){
+	if(isIE8 && type == 'change')
+		type = 'click';
+	if(e.attachEvent)
+		e.attachEvent("on" + type, handler);
+	else /*if(e.addEventListener)*/
+		e.addEventListener(type, handler);
+};
+prevent = function(){
+	if(event.preventDefault)
+		event.preventDefault();
+	else
+		event.returnValue = false;
 };
 
 
 /*_____ Tabs.html _____*/
-
 var doc = document,
 	$news,
 	$srchId,
@@ -161,43 +165,43 @@ var doc = document,
 
 /* news */
 $news = doc.getElementById('news');
-tabsCtrl.init.call($news, $news.getElementsByTagName('h4'));
-tabsCtrl.tabs.call($news , 'click', 'parentElement');
+setItems.call($news, $news.getElementsByTagName('h4'));
+tabs.call($news , 'click', 'parentElement');
 
 /* srchId */
 $srchId = doc.getElementById('srchId');
-tabsCtrl.init.call($srchId, $srchId.getElementsByTagName('input'));
-tabsCtrl.tabs.call($srchId , 'change');  
+setItems.call($srchId, $srchId.getElementsByTagName('input'));
+tabs.call($srchId , 'change');  
 
 /* season */
 $season = doc.getElementById('season');
-tabsCtrl.init.call($season, $season.getElementsByTagName('h4'), true, true);
-tabsCtrl.tabs.call($season , 'click', 'parentElement');
+setItems.call($season, $season.getElementsByTagName('h4'), true, true);
+tabs.call($season , 'click', 'parentElement');
 doc.getElementById('seasonBtnPrev').onclick = function(){
-	tabsCtrl.prevNext.call($season, getDataAttr.call(this, 'direction'));
+	prevNext.call($season, getDataAttr.call(this, 'direction'));
 };
 doc.getElementById('seasonBtnNext').onclick = function(){
-	tabsCtrl.prevNext.call($season, getDataAttr.call(this, 'direction'));
+	prevNext.call($season, getDataAttr.call(this, 'direction'));
 };
 doc.getElementById('seasonBtnPause').onclick = function(){
-	tabsCtrl.pause.call($season);
+	pause.call($season);
 };
 doc.getElementById('seasonBtnPlay').onclick = function(){
-	tabsCtrl.autoPlay.call($season, 3000);
+	autoPlay.call($season, 3000);
 };
-tabsCtrl.autoPlay.call($season, 3000);
+autoPlay.call($season, 3000);
 
 /* menu */
 $menu = doc.getElementById('menu');
-tabsCtrl.init.call(
+setItems.call(
 	$menu, 
 	$menu.getElementsByTagName('li'),
 	true
 );
 doc.getElementById('menuBtnPrev').onclick = function(){
-	tabsCtrl.prevNext.call($menu, 'prev', 3);
+	prevNext.call($menu, 'prev', 3);
 };
 doc.getElementById('menuBtnNext').onclick = function(){
-	tabsCtrl.prevNext.call($menu, 'next', 3);
+	prevNext.call($menu, 'next', 3);
 };
 
